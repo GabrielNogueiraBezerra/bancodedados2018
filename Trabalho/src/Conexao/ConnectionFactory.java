437cc41cce13692d3cfaf90c3ebe1aa79ef30e1b
GabrielNogueiraBezerra <gabrielnogueirabezerra@gmail.com>
2018-05-15 21:36:46 -0300

    Classe de conexão com o banco de dados

---
+++ Trabalho/src/Conexao/ConnectionFactory.java
@@ -0,0 +1,69 @@
+package Conexao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class ConnectionFactory {
+
+    private static ConnectionFactory instancia;
+    private final String DATABASE = "bancoFBD";
+    private final String HOST = "localhost";
+    private final String DRIVE = "com.mysql.cj.jdbc.Driver";
+    private final String URL = "jdbc:mysql://" + HOST
+            + "/" + DATABASE + "?useTimezone=true&serverTimezone=UTC";
+    private final String USER = "root";
+    private final String PASSWORD = "";
+
+    public static ConnectionFactory getInstancia() {
+        if (instancia == null) {
+            instancia = new ConnectionFactory();
+        }
+        return instancia;
+    }
+
+    public Connection getConnection() {
+        try {
+            Class.forName(DRIVE);
+            return DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (SQLException | ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void closeConnection(Connection con) {
+        try {
+            if (con != null) {
+                con.close();
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(ConnectionFactory.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public static void closeConnection(Connection con, PreparedStatement stmt) {
+        closeConnection(con);
+        try {
+            if (stmt != null) {
+                stmt.close();
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(ConnectionFactory.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public static void closeConnection(Connection con, PreparedStatement stmt, ResultSet rs) {
+        closeConnection(con, stmt);
+        try {
+            if (rs != null) {
+                rs.close();
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(ConnectionFactory.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
