3bb825e34e11b989377f4235731b0e119d3d4154
willi <willi@Williana.local>
2018-05-20 11:37:00 -0300

    Coloquei todas as DAOs, e modifiquei os manter das classes modelo. ACHO que o meu buscar das classes DAO não estão certos, preciso que alguem revise!

--- Trabalho/src/Models/InterfaceManter.java
+++ Trabalho/src/Models/InterfaceManter.java
@@ -1,17 +1,20 @@
 package Models;
 
+import java.sql.SQLException;
+
 /**
  *
  * @author Gabriel
+ * @modificado Williana
  */
 public interface InterfaceManter {
     
-    public void inserir();
+    public void inserir()  throws ClassNotFoundException, SQLException;
     
-    public void alterar();
+    public void alterar()  throws ClassNotFoundException, SQLException;
     
-    public void buscar(int codigo);
+    public void buscar(int codigo)  throws ClassNotFoundException, SQLException;
     
-    public void excluir();
+    public void excluir()  throws ClassNotFoundException, SQLException;
     
 }
